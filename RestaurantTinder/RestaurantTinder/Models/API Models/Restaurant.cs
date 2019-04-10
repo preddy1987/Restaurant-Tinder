7ee611b4adf9e0e2d66e64a4199d34b23ec483f6
patrickreddy <patrickreddy87@gmail.com>
2019-04-10 14:59:22 -0400

    updated api models

---
+++ RestaurantTinder/RestaurantTinder/Models/API Models/Restaurant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantTinder.Models.API_Models
+{
+    [Serializable]
+    public class Restaurant
+    {
+        public string Name { get; set; }
+        public string PlaceID { get; set; }
+        public int PriceLevel { get; set; }
+        public int Rating { get; set; }
+        public Array Types { get; set; }
+        public int UserRatingsTotal { get; set; }
+        public string Vicinity { get; set; }
+
+
+    }
+}
