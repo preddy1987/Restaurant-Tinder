b669286d4240793805fcc4977d9143b65685baf8
ascensionmartinez <atm42486@gmail.com>
2019-04-10 19:17:23 -0400

    added register user

--- RestaurantTinder/RestTinderWebAPI/Controllers/UserController.cs
+++ RestaurantTinder/RestTinderWebAPI/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RestaurantTinder.Interfaces;
 using RestaurantTinder.Models;
+using RestTinderWebAPI.Models;
 
 namespace RestTinderWebAPI.Controllers
 {
--- RestaurantTinder/Vue/restaurant-tinder/src/router.js
+++ RestaurantTinder/Vue/restaurant-tinder/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Login from '@/views/Login';
+import Login from './views/Login.vue'
+import Register from './views/Register.vue'
 
 Vue.use(Router);
 
@@ -10,11 +11,15 @@ const router = new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'login',
       component: Login,
-    }
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: Register,
+      }
   ],
 })
-
 
 export default router;
