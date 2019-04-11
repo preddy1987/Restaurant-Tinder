e64da2849145a3a82a7e3922ec1e371d8ba9cbc4
garrickkreitzer <garrickkreitzer1@gmail.com>
2019-04-11 16:01:10 -0400

    DAO Methods

---
+++ RestaurantTinder/RestaurantTinder/Models/Database/RestaurantItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantTinder.Models
+{
+    public class RestaurantItem : BaseItem
+    {
+        public string Name { get; set; }
+
+        public RestaurantItem Clone()
+        {
+            RestaurantItem item = new RestaurantItem();
+            item.Id = this.Id;
+            item.Name = this.Name;
+            return item;
+        }
+    }
+}
