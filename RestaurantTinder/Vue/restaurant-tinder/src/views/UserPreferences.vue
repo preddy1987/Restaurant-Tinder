637c90087185ac13761c621c463e06e9fa5f9833
patrickreddy <patrickreddy87@gmail.com>
2019-04-16 16:22:57 -0400

    changed the sql create file

--- RestaurantTinder/Vue/restaurant-tinder/src/views/UserPreferences.vue
+++ RestaurantTinder/Vue/restaurant-tinder/src/views/UserPreferences.vue
@@ -1,13 +1,13 @@
 <template>
     <div id="preferences">
         <div class="form-preferences " id="current-preferences">
-            <h1 class="h3 mb-3 font-weight-normal" >Your Current Preferences</h1>
+            <h1 class="mb-3 " >Your Current Preferences</h1>
             <ul class="list-group" >
                 <li class="list-group-item" v-for="pref in currentPrefsList" :key="pref.id">{{ pref.name }}</li>
             </ul>
         </div>
         <form class="form-preferences" @submit.prevent="SavePrefs">
-            <h1 class="h3 mb-3 font-weight-normal">Add a food preference here</h1>
+            <h1 class="mb-3">Add a food preference here</h1>
             <div class="alert alert-danger" role="alert" v-if="registrationErrors">
                 There were problems registering this user.
             </div>
@@ -16,7 +16,7 @@
                 type="text"
                 id="preference"
                 class="form-control"
-                placeholder="Mexican"
+                placeholder="e.g. Mexican"
                 v-model="preference"
                 autofocus
             />
@@ -36,22 +36,11 @@ name: 'UserPreferences',
   data() {
     return {
         preference:'',
-        prefsToAdd: [],
         currentPrefs: [],
         registrationErrors: false,
     }
   },
     computed:{
-        prefsToAddList(){
-            let prefs = [];
-                    for(let i = 0; i < this.prefsToAdd.length; i++){                
-                    prefs.push({
-                    id: i,
-                    name: this.prefsToAdd[i].name,       
-                });
-            }
-            return prefs;
-        },
         currentPrefsList(){
             let prefs = [];
                     for(let i = 0; i < this.currentPrefs.length; i++){                
@@ -131,14 +120,27 @@ body {
 #app {
   height: 100%;
 }
+h1 {
+  font-size: 18px!important;
+}
+li{
+  font-size: 14px!important;
+  padding: .25rem .75rem!important;
+}
+button{
+  padding: 0rem!important;
+  font-size: 16px!important;
+  background-color: #ee0979!important;
+  border-color: #ee0979!important;
+}
 .preferences {
   height: 100%;
   display: -ms-flexbox;
   display: flex;
   -ms-flex-align: center;
   align-items: center;
-  padding-top: 40px;
-  padding-bottom: 40px;
+  padding-top: 20px;
+  padding-bottom: 20px;
   background-color: #f5f5f5;
   font-family: 'Roboto Condensed', sans-serif;
 }
@@ -159,18 +161,18 @@ form {
   box-sizing: border-box;
   height: auto;
   padding: 10px;
-  font-size: 16px;
+  font-size: 14px;
 }
 .form-preferences .form-control:focus {
   z-index: 2;
 }
 .form-preferences input[type='text'] {
-  margin-bottom: 10px;
+  margin-bottom: 5px;
   border-bottom-right-radius: 0;
   border-bottom-left-radius: 0;
 }
 #preferences ul{
-  margin-bottom: 10px;
+  margin-bottom: 5px;
   border-bottom-right-radius: 0;
   border-bottom-left-radius: 0;
 }
